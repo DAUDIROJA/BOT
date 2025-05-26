d44f36e8ea88e022c2df9ae98edb806c8731f49f
DAUDIROJA <nyondodaudi27@gmail.com>
2025-05-27 01:34:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,4 +23,4 @@ async def price(ctx):
     await ctx.send("Bitcoin price: $12345 (fake for now!)")
 
 # Use environment variable for token
-bot.run(os.getenv("DISCORD_TOKEN"))
+bot.run(os.getenv("MTM3NjYyMjAxMTA0Mzk0MjQxMA.G_Bkj_.rfSQ-HXoh7BJJl2vV-AhjeucEfg31SPErXB5XQ"))
