1000281048dba1c88bb1285df3de0de138c3e720
DAUDIROJA <nyondodaudi27@gmail.com>
2025-05-27 00:00:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
+import os
 
 # Set up intents
 intents = discord.Intents.default()
-intents.message_content = True  # Allows bot to see message content
-intents.members = True         # Allows bot to see server members
+intents.message_content = True
+intents.members = True
 
 # Create bot with intents
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -21,4 +22,5 @@ async def hello(ctx):
 async def price(ctx):
     await ctx.send("Bitcoin price: $12345 (fake for now!)")
 
-bot.run("MTM3NjYyMjAxMTA0Mzk0MjQxMA.Gxnd3p.0wMaOcmo71vMN2PEpxieeloLjSPzdpcMDWx8ig")
+# Use environment variable for token
+bot.run(os.getenv("MTM3NjYyMjAxMTA0Mzk0MjQxMA.G_iQN4.8RQtBzROEn9hkXcdj-TZVvD3kWmOb55mry-z_Y"))
