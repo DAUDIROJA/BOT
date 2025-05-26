c45ca58a2244d27b92e0bffe684b34b7988b8872
DAUDIROJA <nyondodaudi27@gmail.com>
2025-05-26 23:01:08 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+# Set up intents
+intents = discord.Intents.default()
+intents.message_content = True  # Allows bot to see message content
+intents.members = True         # Allows bot to see server members
+
+# Create bot with intents
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("BOT is ready!")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Yo bro, BOT here to help the fam!")
+
+@bot.command()
+async def price(ctx):
+    await ctx.send("Bitcoin price: $12345 (fake for now!)")
+
+bot.run("MTM3NjYyMjAxMTA0Mzk0MjQxMA.Gxnd3p.0wMaOcmo71vMN2PEpxieeloLjSPzdpcMDWx8ig")
